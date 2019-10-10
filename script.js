1c2bc470e9f472119425f3c1a08cfe17d5d24519
Carolista <55961845+Carolista@users.noreply.github.com>
2019-10-09 23:08:24 -0500

    Empty field validator working

--- script.js
+++ script.js
@@ -11,3 +11,38 @@
 </ol>
 <img src="${}">
 */
+
+
+// VALIDATORS
+
+window.addEventListener("load", function() {
+
+    let form = document.getElementById("formTag"); // reference form
+
+    form.addEventListener("submit", function(event) {   
+
+        console.log("Form event handler initiated.");
+
+        // Reference all four fields
+        let pilot = document.querySelector("input[name = pilotName]");
+        let copilot = document.querySelector("input[name = copilotName]");
+        let fuel = document.querySelector("input[name = fuelLevel]");
+        let cargo = document.querySelector("input[name = cargoWeight]");
+        console.log(pilot, copilot, fuel, cargo); // data check
+
+        // Are any of the fields empty?
+        if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
+            alert("All fields required.");
+            event.preventDefault();
+        }
+
+        // Are the pilot's and co-pilot's names something other than strings?
+        // THIS IS NOT WORKING YET... FIND BETTER WAY TO ENSURE CAN'T BE CONVERTED TO STRING
+        if ( typeof String(pilot.value) !== 'string' || typeof String(copilot.value) !== 'string') {
+            alert("Oops! Please check that you have entered valid names.");
+            event.preventDefault();
+        }
+
+    });
+
+});
